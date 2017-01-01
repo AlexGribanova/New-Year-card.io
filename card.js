94f27ce667fe0200edd9cf6a6f4840d68c56f260
Sasha_frontend <shura_gribanova@mail.ru>
2017-01-01 17:49:00 +0300

    Новогодняя открытка

---
+++ card.js
@@ -0,0 +1,47 @@
+!function ($) {
+	var $snowflakes = $('#snowflakes'),
+	    snowflakesCount = $snowflakes.data('count');
+	
+	setInterval(showSnowFlakes, 5000);
+
+	function showSnowFlakes(){
+		for (var i = 0; i < snowflakesCount; i++) {
+			!function(){
+				var $snowflake = $('<div class="snowflake"></div>').appendTo($snowflakes);
+				var left = Math.round(Math.random()*100);
+				var top = -Math.round(Math.random()*40);
+				$snowflake.css('left', left+'%');
+				$snowflake.css('top', top+'%');
+
+				setTimeout(function() {
+				$snowflake.css('top', top+140+'%');
+				}, 100);
+
+				setTimeout(function() {
+				$snowflake.remove();
+				}, 20000);
+			}(i)
+		}
+	}
+
+	$(document).ready(function() {
+
+    var $play = $('#audio-play'),
+        $pause = $('#audio-pause'),
+        song = $('#song').get(0);
+
+      $play.on('click', function() {
+        song.play();
+        $play.addClass('hidden');
+        $pause.removeClass('hidden');
+      });
+
+      $pause.on('click', function() {
+        song.pause();
+        $pause.addClass('hidden');
+        $play.removeClass('hidden');
+      });
+  });
+
+}(jQuery)
+
